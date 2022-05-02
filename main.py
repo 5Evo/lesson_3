5cf6e7a8259a211f3482a72546d63d692edd0e9d
aleks <aleksei.petaikin@gmail.com>
2022-05-02 14:19:03 +0500

    result commit

---
+++ main.py
@@ -0,0 +1,36 @@
+"""
+Наиболее популярные методы на мой взгляд:
+
+Методы списков
+1.	Создание списка list()
+2.	list.len()
+3.	list.append(x)
+4.	list.extend(L)
+5.	list.count(x)
+6.	list.sort([key=функция])
+
+Методы словарей
+1.	Создагние словаря dic()
+2.	dict.len()
+3.	dict.values()
+4.	dict.keys()
+5.	dict.items()
+6.	dict.fromkeys(keys, value)
+
+Методы множеств
+1.	Создание множества set()
+2.	len(s) – длина множества
+3.	set.update(other, ...) – объединение
+4.	set.intersection(other, ...) – пересечение
+5.	set.difference(other, ...) – вычитание множеств
+6.	set.add(elem) – добавление элемента
+
+Методы строк
+1.	len(S) длина строки
+2.	S.find(str, [start],[end]) – поиск вхождения
+3.	S.replace(шаблон, замена[, maxcount]) – замена части строки
+4.	S.split(символ) – разбиение строки по символу
+5.	S.isdigit(), S.isalpha(), S.isalnum() – проверка строк на буквы и цифры
+6.	S.upper(), S.lower() – преобразование к верхнему/нижнему регистру
+
+"""
